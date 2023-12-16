6ffd5fda12ccdb73295d0d2101e1f69ea3062fb0
Terlan14 <terlanibrahimli37@gmail.com>
2023-12-16 12:07:25 +0400

    good

---
+++ DaoPattern/src/main/java/com/atashgah/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.atashgah.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atashgah.Repository.UserRepository;
+import com.atashgah.model.User;
+
+@RestController
+
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@GetMapping("users")
+	public List<User >getAllUsers() {
+		return userRepository.findAll();
+	}
+
+}
---
+++ DaoPattern/src/main/java/com/atashgah/model/User.java
@@ -0,0 +1,74 @@
+package com.atashgah.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name="pattern")
+public class User {
+	
+	//user variables
+	@Id
+	private Integer id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email")
+	private String email;
+	@Column(name="password")
+	private String password;
+	
+	
+	
+	public User() {
+		super();
+	}
+	public User(Integer id, String firstName, String lastName, String email, String password) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+
+}
